02577a564418d5461d5e5f46bcb81aa982f5d391
Klepaski <43060010+klepaski@users.noreply.github.com>
2020-06-09 21:37:42 +0300

    Add files via upload

---
+++ 14 - Таймер/14-timer/Resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_MY14TIMER_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_MY14TIMER			107
+#define IDI_SMALL				108
+#define IDC_MY14TIMER			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#define IDT_TIMER1			    122
+#define IDT_TIMER4			    125
+#define IDT_TIMER2			    123
+#define IDT_TIMER3			    124
+#define IDT_TIMER4			    125
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
