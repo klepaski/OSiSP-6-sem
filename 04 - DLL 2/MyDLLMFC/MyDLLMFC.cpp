9751e9d8a184b9a306b41e20d6c0ba42c31f72bd
Klepaski <43060010+klepaski@users.noreply.github.com>
2020-06-09 21:11:08 +0300

    Add files via upload

---
+++ 04 - DLL 2/MyDLLMFC/MyDLLMFC.cpp
@@ -0,0 +1,77 @@
+﻿// MyDLLMFC.cpp: определяет процедуры инициализации для библиотеки DLL.
+//
+
+#include "stdafx.h"
+#include "MyDLLMFC.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+__declspec(dllexport) int SquareInt(int param);
+__declspec(dllexport) int SendMessage();
+
+__declspec(dllexport) int Sum(int a, int b);
+__declspec(dllexport) int Mul(int a, int b);
+__declspec(dllexport) int Div(int a, int b);
+
+int SquareInt(int param)
+{
+	return param * param;
+}
+
+int SendMessageNumber()
+{
+	return 100;
+}
+
+int Sum(int a, int b)
+{
+	return a + b;
+}
+
+int Mul(int a, int b)
+{
+	return a * b;
+}
+
+int Div(int a, int b)
+{
+	return a / b;
+}
+
+
+
+#pragma region
+
+// CMyDLLMFCApp
+
+BEGIN_MESSAGE_MAP(CMyDLLMFCApp, CWinApp)
+END_MESSAGE_MAP()
+
+
+// Создание CMyDLLMFCApp
+
+CMyDLLMFCApp::CMyDLLMFCApp()
+{
+	// TODO: добавьте код создания,
+	// Размещает весь важный код инициализации в InitInstance
+}
+
+
+// Единственный объект CMyDLLMFCApp
+
+CMyDLLMFCApp theApp;
+
+
+// Инициализация CMyDLLMFCApp
+
+BOOL CMyDLLMFCApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+	return TRUE;
+}
+
+#pragma endregion
---
+++ 04 - DLL 2/MyDLLMFC/MyDLLMFC.h
@@ -0,0 +1,27 @@
+﻿// MyDLLMFC.h: основной файл заголовка для библиотеки DLL MyDLLMFC
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CMyDLLMFCApp
+// Реализацию этого класса см. в файле MyDLLMFC.cpp
+//
+
+class CMyDLLMFCApp : public CWinApp
+{
+public:
+	CMyDLLMFCApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
