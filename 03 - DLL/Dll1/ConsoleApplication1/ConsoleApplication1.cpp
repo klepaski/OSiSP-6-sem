537abe68c9c796178efc2ffca0a89307ef8dff87
Klepaski <43060010+klepaski@users.noreply.github.com>
2020-06-09 21:07:16 +0300

    Add files via upload

---
+++ 03 - DLL/Dll1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "lab1.h"
+#include <Windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	WriteFile();
+	StartProc();
+	CheckFile();
+
+	system("pause");
+}
